172ef92d94e782123f4390754a4ec07de9c2e438
Tich Kadzinga <tichaona@kadzinga.com>
2022-08-31 10:33:59 +0200

    Fix logical errors in constituent encoder

--- README.md
+++ README.md
@@ -3,13 +3,14 @@
 This is an implementation of the 4G LTE eNodeB and 5G New Radio gNB physical layer in Chisel. 
 The goal is to provide gateware for use with off the shelf software defined radio front ends like bladeRF.
 
-## Roadmap
+## Milestones
 #### 4G LTE Downlink TX:
 - [ ] PDSCH:
   - [x] CRC (Compute and validate)
   - [ ] Turbo Coding:
     - [ ] Constituent Encoder
-    - [ ] Interleaver 
+    - [ ] Interleaver
+    - [ ] Turbo encoder finite state machine 
   - [ ] Rate Matching
   - [ ] Code block concatenation
   - [ ] Scrambling
--- src/main/scala/lte/fec/TurboEncoder.scala
+++ src/main/scala/lte/fec/TurboEncoder.scala
@@ -4,27 +4,23 @@ import chisel3._
 class ConstituentEncoder() extends Module {
   val io = IO(new Bundle {
     val dataIn = Input(Bool())
-    val valid = Input(Bool())
-    val tailingValid = Input(Bool())
+    val parityEnable = Input(Bool())
+    val tailingEnable = Input(Bool())
     val dataOut = Output(Bool())
   })
 
-  val shiftReg1 = RegInit(false.B)
-  val shiftReg2 = RegInit(false.B)
-  val shiftReg3 = RegInit(false.B)
-  val dataInReg = RegInit(io.dataIn)
+  val shiftReg1  = RegInit(false.B)
+  val shiftReg2  = RegInit(false.B)
+  val shiftReg3  = RegInit(false.B)
 
-  dataInReg     := io.dataIn ^ shiftReg2 ^ shiftReg3
-  io.dataOut    :=  dataInReg ^ shiftReg1 ^ shiftReg3
-
-  when(io.valid) {
-    shiftReg1 := dataInReg ^ shiftReg1 ^ shiftReg3
-    shiftReg2 := shiftReg1
-    shiftReg3 := shiftReg2
+  io.dataOut    :=  io.dataIn ^ shiftReg2 ^ shiftReg3 ^ shiftReg1 ^ shiftReg3
+  shiftReg2 := shiftReg1
+  shiftReg3 := shiftReg2
+  when(io.parityEnable) {
+    shiftReg1     := io.dataIn ^ shiftReg2 ^ shiftReg3
   }
-  when(io.valid & io.tailingValid){
-    shiftReg1 := false.B
-    shiftReg2 := shiftReg1
-    shiftReg3 := shiftReg2
+  when(io.tailingEnable){
+    io.dataOut    :=  io.dataIn ^ shiftReg1 ^ shiftReg3
+    shiftReg1     := false.B
   }
 }
